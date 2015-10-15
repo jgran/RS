0fd060800d6b243a46edb18f8b18a31cae07ef89
jgran <jgran@physics.ucsb.edu>
2015-10-15 15:28:09 -0700

    updates

--- modifyResponse/do_modification.C
+++ modifyResponse/do_modification.C
@@ -21,9 +21,9 @@ double ComputeGaussianValue(double scale, double mean, double sigma, float x){
 void do_modification(){
 
   //core + 25%
-  //float core_mod = 1.25;
-  //float tail_mod = 1.0;
-  //std::string plotdir = "core_plus_25percent";
+  float core_mod = 1.25;
+  float tail_mod = 1.0;
+  std::string plotdir = "core_plus_25percent";
 
   //core - 25%
   //float core_mod = 0.75;
@@ -36,9 +36,9 @@ void do_modification(){
   //std::string plotdir = "tail_plus_25percent";
 
   //tail - 25%
-  float core_mod = 1.0;
-  float tail_mod = 0.75;
-  std::string plotdir = "tail_minus_25percent";
+  //float core_mod = 1.0;
+  //float tail_mod = 0.75;
+  //std::string plotdir = "tail_minus_25percent";
 
   TFile* outfile_fits = new TFile(Form("template_fits_%s.root", plotdir.c_str()), "RECREATE");
   TFile* outfile_templates = new TFile(Form("templates_%s.root", plotdir.c_str()), "RECREATE");
@@ -48,7 +48,6 @@ void do_modification(){
   TIter it(responseFile->GetListOfKeys());
   TKey* key;
   std::string keep = "h_tot_JetAll_ResponsePt";
-  //std::string keep = "h_tot_JetAll_ResponsePt_Pt6_Eta9";
   //std::string keep = "h_tot_JetAll_ResponsePt_Pt0_Eta0";
   while ( (key = (TKey *)it()) ) {
     if (strncmp (key->GetTitle(), keep.c_str(), keep.length()) != 0) continue;
@@ -95,8 +94,9 @@ void do_modification(){
     h_tail->Draw("same");
     h_core->SetLineColor(kRed);
     h_core->Draw("same");
-    c1->Print(Form("plots/core_tail_%s.pdf", hist_name.c_str()));
+    c1->Print(Form("plots/core_mod_%s.pdf", hist_name.c_str()));
     c1->Clear();
+
 
     for(int i=1; i<=h_new_core->GetNbinsX(); i++){
       float gaussian_contribution = ComputeGaussianValue(g_scale, g_mean, core_mod*g_sigma, h_new_core->GetBinCenter(i));
@@ -106,20 +106,54 @@ void do_modification(){
       h_new_tail->SetBinContent(i, tail_mod*h_tail->GetBinContent(i)); 
     }
 
+    //rescale new core histogram to have same area as original core
+    float core_integral_before = h_core->Integral();
+    float core_integral_after = h_new_core->Integral();
+    float integral_scale_factor = core_integral_before/core_integral_after;
+    h_new_core->Scale(integral_scale_factor);
+
     outfile_fits->cd();
     fit->SetParameter(2, core_mod*g_sigma);
+    fit->SetParameter(0, integral_scale_factor*g_sigma);
     fit->Write();
 
     delete fit;
     delete c1;
     TCanvas* c1 = new TCanvas();
+    c1->SetLogy();
 
     h_new_core->Add(h_new_tail, 1.0);
-    h_new_core->Scale(1.0/h_new_core->Integral());
-    h_new_core->SetLineColor(kRed);
-    h_new_core->Draw();
-    //h_tail->Draw("same");
-    //h->Draw("same");
+    //h_new_core->Scale(1.0/h_new_core->Integral());
+    std::cout << "h_new_core->Integral() = " << h_new_core->Integral() << std::endl;
+    h_core->SetMarkerColor(kRed);
+    h_core->SetMarkerStyle(20);
+    h_core->SetMarkerSize(0.8);
+    h_tail->SetMarkerColor(kGreen);
+    h_tail->SetMarkerStyle(20);
+    h_tail->SetMarkerSize(0.8);
+    h_new_core->SetMarkerStyle(20);
+    h_new_core->SetMarkerSize(0.8);
+    h->SetFillColor(kGray+1);
+    h->SetLineColor(kGray+1);
+    //h->SetMaximum(1.2 * std::max(h_new_core->GetMaximum(), h->GetMaximum()));
+    h->SetMaximum(2.0 * std::max(h_new_core->GetMaximum(), h->GetMaximum()));
+    h->SetMinimum(0.0001);
+    h->Draw("H");
+    h_core->Draw("SAMEL");
+    h_tail->Draw("SAMEL");
+    h_new_core->Draw("SAME");
+    TLegend* legend = new TLegend(0.6, 0.6, 0.85, 0.85);
+    legend->SetBorderSize(0);
+    legend->SetTextSize(0.03);
+    legend->SetTextFont(42);
+    legend->SetFillColor(0);
+    legend->AddEntry( h, "Original Template", "f" );
+    if(plotdir == "core_plus_25percent") legend->AddEntry( h_new_core, "New Template (width + 25%)", "p" );
+    else if(plotdir == "core_minus_25percent") legend->AddEntry( h_new_core, "New Template (width - 25%)", "p" );
+    else legend->AddEntry( h_new_core, "New Template", "p" );
+    legend->AddEntry( h_core, "Original Core", "p" );
+    legend->AddEntry( h_tail, "Original Tail", "p" );
+    legend->Draw("SAME");
     c1->Print(Form("%s/new_core_%s.pdf", plotdir.c_str(), hist_name.c_str()));
     outfile_templates->cd();
     h_new_core->Write(hist_name.c_str());
